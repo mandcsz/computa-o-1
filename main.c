2458163c6ca9ccb48a21690883ca32898db84f42
mandcsz <amandachrisssousa@gmail.com>
2025-08-13 11:20:01 -0300

    imc

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+    printf ("Calculo do IMC \n") ;
+    printf ("Altura: %f \n", 1.64) ;
+    printf ("Peso:%f \n", 62.5) ;
+
+    printf ("IMC: %f ", 62.5/(1.64* 1.64)) ;
+    
+    return 0 ;
+}
