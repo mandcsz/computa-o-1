73261e889940a7010212ff265364d66f19736a33
mandcsz <amandachrisssousa@gmail.com>
2025-09-10 09:58:56 -0300

    Add files via upload

---
+++ prova1_ex1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+#define PRIMEIRA 1
+#define ULTIMA 1 
+
+	int quantidade_1;
+	int quantidade_2;
+	float valor;
+
+	printf("digite a quantidade_1:");
+	scanf("%d", &quantidade_1);
+
+	printf("digite a quantidade_2:");
+	scanf("%d", &quantidade_2);
+
+	valor = (quantidade_1 + quantidade_2) * (ULTIMA - PRIMEIRA / 2.0);
+
+	if (valor > quantidade_1) {
+		printf("O resultado é %f e as quantidades sao %d e %d", valor, quantidade_1, quantidade_2);
+	}
+	else {
+		valor = valor * 0.8;
+		printf("O resultado é %f e as quantidades sao %d e %d", valor * 0.8, quantidade_1, quantidade_2);
+	}
+	return 0;
+}
